8bc76338681163ac9aa610a78fbbb1e5124430ff
dianasalinas03 <admin@uacj-892f902bd6.uacj.mx>
2025-02-12 11:49:18 -0700

    Catalogo de productos

---
+++ app.js
@@ -0,0 +1,33 @@
+const items = document.getElementById('items');
+const templateCard = document.getElementById('template-card').content;
+const fragmento = document.createDocumentFragment();
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData();
+});
+
+const fetchData = async () => {
+    try {
+        const res = await fetch('app.json'); // Asegúrate de que la ruta es correcta
+        const data = await res.json();
+        console.log(data);
+        mostrarProductos(data);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const mostrarProductos = (data) => {
+    data.forEach((producto) => {
+        templateCard.querySelector('h3').textContent = producto.nombre;
+        templateCard.querySelector('p').textContent = producto.precio;
+            const imgUrl=producto.imgUrl || producto.imgUrl;
+        templateCard.querySelector('img').setAttribute('src',imgUrl);
+            
+
+        const clone = templateCard.cloneNode(true);
+        fragmento.appendChild(clone); // Aquí estaba el error tipográfico
+    });
+    items.appendChild(fragmento);
+};
+
